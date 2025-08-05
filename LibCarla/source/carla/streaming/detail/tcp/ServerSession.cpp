9f706efd1169f1a1ae937403ed2a46e27a3f073c
jaeger <B.Jaeger1@gmx.net>
2025-08-05 23:34:01 +0200

    Try to log stacktrace.

--- LibCarla/source/carla/streaming/detail/tcp/ServerSession.cpp
+++ LibCarla/source/carla/streaming/detail/tcp/ServerSession.cpp
@@ -18,6 +18,10 @@
 #include <atomic>
 #include <thread>
 
+// TODO BE debug
+#include <boost/stacktrace.hpp>
+#include <iostream>
+
 namespace carla {
 namespace streaming {
 namespace detail {
@@ -62,7 +66,12 @@ namespace tcp {
           boost::asio::post(_strand.context(), [=]() { callback(self); });
         } else {
           log_error("session", _session_id, ": error retrieving stream id :", ec.message());
+          // TODO BE debug
+          std::cerr << "LibCARLA ServerSession.cpp:\n" << std::endl;
+          std::cerr << "Stack trace:\n" << boost::stacktrace::stacktrace() << std::endl;
+          std::cerr << "Before CloseNow:\n" << std::endl;
           CloseNow(ec);
+          std::cerr << "After CloseNow:\n" << std::endl;
         }
       };
 
@@ -134,6 +143,7 @@ namespace tcp {
   }
 
   void ServerSession::CloseNow(boost::system::error_code ec) {
+    // TODO BE: The socket is only shutdown when there is no error. What happens when there is an error?
     _deadline.cancel();
     if (!ec)
     {
