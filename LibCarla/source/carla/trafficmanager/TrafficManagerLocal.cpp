e3c608212420ce31bb3b7c5046482e44e76db786
jaeger <B.Jaeger1@gmx.net>
2025-08-09 03:31:53 +0200

    update

--- LibCarla/source/carla/trafficmanager/TrafficManagerLocal.cpp
+++ LibCarla/source/carla/trafficmanager/TrafficManagerLocal.cpp
@@ -160,12 +160,12 @@ void TrafficManagerLocal::Run() {
 
     // Wait for external trigger to initiate cycle in synchronous mode.
     if (synchronous_mode) {
-      tm_done.store(false);
       tm_rdy.store(true);
       std::cout << "590" << std::endl;  // TODO BE debug
       while (!tm_tick.load() and run_traffic_manger.load() and tm_synchronous.load()) {
         std::this_thread::yield(); // Wait for tick.
       }
+      tm_done.store(false);
       std::cout << "tm_tick: " << tm_tick.load() << "run_traffic_manger: " << run_traffic_manger.load() << "tm_synchronous: " << tm_synchronous.load() << std::endl;  // TODO BE debug
       tm_tick.store(false);
       std::cout << "999" << std::endl;  // TODO BE debug
